3ac4cf95cfc4b18a5e3197468215b159969f0a29
atakanteko <atakantekoglu@gmail.com>
2024-06-29 23:15:30 +0300

    feat: create note dto and new note endpoint

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { CreateNoteDto } from "./dto/create-note.dto";
+import { Note } from "./schemas/note.schema";
+import { NoteService } from "./note.service";
 
-@Controller('note')
-export class NoteController {}
+@Controller("note")
+export class NoteController {
+  constructor(private readonly noteService: NoteService) {}
+
+  @Post("new")
+  async createBook(@Body() note: CreateNoteDto): Promise<Note> {
+    return this.noteService.create(note);
+  }
+}
--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -2,8 +2,14 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Note } from "./schemas/note.schema";
 import { Model } from "mongoose";
+import { CreateNoteDto } from "./dto/create-note.dto";
 
 @Injectable()
 export class NoteService {
   constructor(@InjectModel(Note.name) private noteModel: Model<Note>) {}
+
+  async create(book: Note): Promise<Note> {
+    const res = await this.noteModel.create(book);
+    return res;
+  }
 }
